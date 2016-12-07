220cb963de1938623ad015f6450f2d5650d69b21
wuyutaott <wuyutaoktm@163.com>
2016-12-07 20:21:03 +0800

    修改 timezone => ‘Asia/Shanghai’

--- app/Http/Controllers/MsgController.php
+++ app/Http/Controllers/MsgController.php
@@ -7,50 +7,114 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use DB;
+use App\Msg;
+use App\Test;
 
 class MsgController extends Controller
 {
-    // 增加留言
-    public function add() {
+    // // 增加留言
+    // public function add() {
+    //     if (empty($_POST)) {
+    //         return view('msg.add');   
+    //     } else {
+    //         $data = ['title'=>$_POST['title'], 'content'=>$_POST['content']];
+    //         $rs = DB::table('msgs')->insert($data);
+    //         return redirect('msg/index');
+    //     }
+    // }
+
+    // // 删除留言
+    // public function del($id) {
+    //     $rs = DB::table('msgs')->where('id',$id)->delete();
+    //     if ($rs) {
+    //         return redirect('msg/index');
+    //     } else {
+    //         echo 'fail';
+    //     }
+    // }    
+
+    // // 查看留言
+    // public function index() {
+    //     $msg = DB::table('msgs')->get();
+    //     return view('msg.index', ['msg'=>$msg]);
+    // }
+
+    // // 修改留言
+    // public function up($id) {
+    //     if (empty($_POST)) {
+    //         $row = DB::table('msgs')->where('id', $id)->first();
+    //         return view('msg.up', ['row'=>$row]);   
+    //     } else {
+    //         $data = ['title'=>$_POST['title'], 'content'=>$_POST['content']];
+    //         $rs = DB::table('msgs')->where('id',$id)->update($data);
+    //         if ($rs) {
+    //             return redirect('msg/index');
+    //         } else {
+    //             echo 'fail';
+    //         }
+    //     }
+    // }
+    
+    // 添加
+    public function add(Request $req) {
         if (empty($_POST)) {
             return view('msg.add');   
         } else {
-            $data = ['title'=>$_POST['title'], 'content'=>$_POST['content']];
-            $rs = DB::table('msgs')->insert($data);
-            return redirect('msg/index');
+            $msg = new Msg();
+            $msg->title = $_POST['title'];
+            $msg->content = $_POST['content'];
+            $rs = $msg->save();
+            return $rs ? 'ok' : 'fail';
         }
     }
 
-    // 删除留言
+    //查
+    public function index() {
+        $msg = Msg::where('id',1);
+        // $msg = Msg::all();
+        // $msg = Msg::orderBy('id', 'desc')->get(); //倒序
+        // $msg = Msg::where('id','>',1)->skip(1)->take(1)->get();
+        return view('msg.index', ['msg'=>$msg]);
+    }
+
+    //查
+    public function test() {
+        // $msg = Msg::find(3);
+        // $msg = Msg::where('id','=',1)->first();
+        // $msg = Msg::all(['title', 'content']);
+        // $msg = Msg::where('id', '>', 1)->get(['title', 'content']);
+        $msg = Msg::where('id','=',1)->select('title')->get();
+
+        echo '<pre>';
+        print_r($msg);
+    }
+
+    public function up(Request $req, $id) {
+        if (empty($_POST)) {
+            $msg = Msg::find($id);
+            return view('msg.up', ['row'=>$msg]);
+        } else {
+            // dd($req);
+            $msg = Msg::find($id);
+            $msg->title = $req->input('title');
+            $msg->content = $req->content;
+            $msg->age = $req->input('age', 11); //有自动填充作用
+            $rs = $msg->save();
+            return $rs ? 'ok' : 'fail';
+        }
+    }
+
     public function del($id) {
-        $rs = DB::table('msgs')->where('id',$id)->delete();
-        if ($rs) {
+        $msg = Msg::where('id',$id)->delete();
+        if ($msg) { 
             return redirect('msg/index');
         } else {
             echo 'fail';
         }
-    }    
-
-    // 查看留言
-    public function index() {
-        $msg = DB::table('msgs')->get();
-        return view('msg.index', ['msg'=>$msg]);
     }
 
-    // 修改留言
-    public function up($id) {
-        if (empty($_POST)) {
-            $row = DB::table('msgs')->where('id', $id)->first();
-            return view('msg.up', ['row'=>$row]);   
-        } else {
-            $data = ['title'=>$_POST['title'], 'content'=>$_POST['content']];
-            $rs = DB::table('msgs')->where('id',$id)->update($data);
-            if ($rs) {
-                return redirect('msg/index');
-            } else {
-                echo 'fail';
-            }
-        }
+    public function show(Request $req) {
+        dd($req);
     }
 }
 
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,20 +15,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('add', 'TestController@add');
-Route::get('up', 'TestController@up');
-Route::get('del', 'TestController@del');
-Route::get('show', 'TestController@show');
+// Route::get('add', 'TestController@add');
+// Route::get('up', 'TestController@up');
+// Route::get('del', 'TestController@del');
+// Route::get('show', 'TestController@show');
 
 // 留言路由
 Route::match(['get', 'post'], 'msg/add', 'MsgController@add'); //增
 Route::get('msg/del/{id}', 'MsgController@del'); //删
 Route::get('msg/index', 'MsgController@index'); //查
 Route::match(['get', 'post'], 'msg/up/{id}', 'MsgController@up'); //改
+Route::get('msg/test', 'MsgController@test');
 
-// 模版操作
-Route::get('test', 'TestController@test');
-Route::get('test1', 'TestController@test1');
+// // 模版操作
+// Route::get('test', 'TestController@test');
+// Route::get('test1', 'TestController@test1');
+
+Route::get('msg/show', 'MsgController@show');
+
+Route::get('t', 'TestFunc@test');
 
 
 
