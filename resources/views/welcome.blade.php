af31e7abb0c823bdc90bcd73f454189d61d86178
wuyutaott <wuyutaoktm@163.com>
2016-12-08 21:53:13 +0800

    修改welcome
    修改文案

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello World!</div>
+                <div class="title">Hello laravel!</div>
             </div>
         </div>
     </body>
